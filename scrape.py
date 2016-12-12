b846129c2e624b45637633a89ca2dea65326b430
Vicky Chen <c.vicky1213@gmail.com>
2016-12-12 14:55:59 -0500

    format the info into lists

--- scrape.py
+++ scrape.py
@@ -6,4 +6,15 @@ response = requests.get(url)
 html = response.content
 
 soup = BeautifulSoup(html)
-print soup.prettify()
+table = soup.find('tbody', attrs = {'class': 'stripe'})
+
+list_of_rows = []
+for row in table.findAll('tr'):
+	list_of_cells = []
+	for cell in row.findAll('td'):
+		#get rid of all the annoying non-breaking space codes
+		text = cell.text.replace('&nbsp', '') 
+		list_of_cells.append(text)
+	list_of_rows.append(list_of_cells)
+
+print list_of_rows
